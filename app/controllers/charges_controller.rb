cdb4c75d152a610281ff369a0347fb5efcb906e0
Shay-Jahen Merritte <Shay@Merritte.com>
2014-09-11 23:40:52 -0400

    added the ability to store purchases and display your purchase

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -16,7 +16,13 @@ class ChargesController < ApplicationController
 	    :currency    => 'usd'
 	  )
 
-		rescue Stripe::CardError => e
+	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
+    amount: params[:amount], description: charge.description, currency: charge.currency,
+    customer_id: customer.id, product_id: 1)
+
+	 redirect_to purchase
+
+	rescue Stripe::CardError => e
 	  flash[:error] = e.message
 	  redirect_to charges_path
 	end
